7c92b547c21e0599a8ef16b9f9b77c7b0ca6c872
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-29 10:10:12 -0300

    modulo 2-boolean

---
+++ src/Modulo-02/Boolean/Boolean.js
@@ -0,0 +1,10 @@
+"use strict";
+let tarefaConcluida = true;
+let tarefaPendente = false;
+let concluida = false;
+if (!concluida) {
+    console.log('Tarefa foi concluída com sucesso!');
+}
+else {
+    console.log('Tarefa pendente!');
+}
---
+++ src/Modulo-02/Boolean/Boolean.ts
@@ -0,0 +1,10 @@
+ let tarefaConcluida:boolean = true;
+ let tarefaPendente : boolean= false;
+let concluida: boolean = false
+
+if(!concluida){
+console.log('Tarefa foi concluída com sucesso!')
+
+}else{
+    console.log('Tarefa pendente!')
+}
