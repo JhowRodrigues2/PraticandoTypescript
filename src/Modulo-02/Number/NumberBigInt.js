0a7766e7aeb9d4685efb095d7c83dee8128efcf3
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-29 10:21:53 -0300

    modulo 2-int

---
+++ src/Modulo-02/Number/NumberBigInt.js
@@ -0,0 +1,6 @@
+"use strict";
+let num1 = 23.0;
+let num2 = 0x78cf;
+let num3 = 0b110001;
+// Bigint
+//let big1 : bigint = 13899839180391391328n;
---
+++ src/Modulo-02/Number/NumberBigInt.ts
@@ -0,0 +1,7 @@
+let num1: number=23.0;
+let num2: number=0x78cf;
+let num3: number=0b110001;
+
+// Bigint
+
+//let big1 : bigint = 13899839180391391328n;
