fefd8cd77425e87223360bfaf25cfbd684083cdb
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-29 11:03:34 -0300

    modulo 2-Array

---
+++ src/Modulo-02/Array/Array.js
@@ -0,0 +1,17 @@
+"use strict";
+// Uso dos colchetes=>
+let frutas = ['mamão', 'abacaxi', 'manga', 'banana'];
+console.log(frutas);
+// Map
+frutas.map((item) => (console.log(item)));
+// Array Object =>
+let frutasObject = ['mamão', 'abacaxi', 'manga', 'banana'];
+// Metodo Push => Adiciona Item no Array
+let idiomas = ['Portugues', 'Inglês', 'Espanhol', 'Italiano'];
+idiomas.push('Frances');
+console.log(idiomas);
+// Método Length
+console.log(`Você sabe falar ${idiomas.length} idiomas.`);
+//Spread Operator
+let listaNum = [1, 2, 3, 4, 5];
+listaNum = [...listaNum, 6, 7, 8, 9, 10];
