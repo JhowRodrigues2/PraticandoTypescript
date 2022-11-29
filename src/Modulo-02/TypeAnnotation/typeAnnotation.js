ada2024de5c7b5ac721de508319912505a07ae38
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-29 09:53:42 -0300

    modulo 2

---
+++ src/Modulo-02/TypeAnnotation/typeAnnotation.js
@@ -0,0 +1,13 @@
+"use strict";
+let nome = 'Jhow Rodrigues';
+//array [type Annotations] =>
+let animais = ['cachorro', 'gato', 'papagaio', 'macaco', 'panda'];
+// Objeto
+let carro;
+carro = { nome: 'Gol', ano: 2012, preco: 50.000 };
+console.log(carro.nome);
+// function
+function Somar(num1, num2) {
+    return num1 + num2;
+}
+console.log(Somar(3, 3));
---
+++ src/Modulo-02/TypeAnnotation/typeAnnotation.ts
@@ -0,0 +1,20 @@
+let nome: string ='Jhow Rodrigues'
+//array [type Annotations] =>
+let animais:string[]=['cachorro','gato','papagaio','macaco','panda']
+
+
+// Objeto
+let carro:{
+nome:string,
+ano:number,
+preco:number
+}
+
+carro={nome:'Gol',ano:2012, preco:50.000}
+console.log(carro.nome)
+
+// function
+ function Somar(num1:number, num2:number){
+    return num1 +num2
+ }
+ console.log(Somar(3,3))
