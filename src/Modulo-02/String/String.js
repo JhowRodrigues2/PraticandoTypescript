7b80098966bbb400475da6b94f905d4428b5696d
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-29 10:33:31 -0300

    modulo 2-string

---
+++ src/Modulo-02/String/String.js
@@ -0,0 +1,6 @@
+"use strict";
+let nomeCompleto = 'Jhonatta Rodrigues Santos';
+let funcaoEmpresa = "Desenvolvedor Front-End";
+let nomeEmpresa = 'Microsoft';
+let dadosFuncionario = `nome:${nome} - Função:${funcaoEmpresa} - Empresa:${nomeEmpresa}`;
+console.log(dadosFuncionario);
---
+++ src/Modulo-02/String/String.ts
@@ -0,0 +1,7 @@
+let nomeCompleto: string = 'Jhonatta Rodrigues Santos'
+let funcaoEmpresa: string = "Desenvolvedor Front-End"
+let nomeEmpresa: string = 'Microsoft'
+
+let dadosFuncionario = `nome:${nome} - Função:${funcaoEmpresa} - Empresa:${nomeEmpresa}`
+
+console.log(dadosFuncionario)
