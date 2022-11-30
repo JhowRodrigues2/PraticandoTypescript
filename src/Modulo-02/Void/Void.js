2f73635d4fd4ba124b06121b09e267dc64933c8d
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-30 10:49:03 -0300

    modulo 2-Void

---
+++ src/Modulo-02/Void/Void.js
@@ -0,0 +1,7 @@
+"use strict";
+// Void será usado quando a função não tiver nenhum retorno.
+// exemplo - Funções 
+const logError = (erroMessage) => {
+    console.log(erroMessage);
+};
+logError('Required filed- Name!');
---
+++ src/Modulo-02/Void/Void.ts
@@ -0,0 +1,8 @@
+// Void será usado quando a função não tiver nenhum retorno.
+
+// exemplo - Funções 
+
+const logError=(erroMessage:string):void=>{
+console.log(erroMessage)
+}
+logError('Required filed- Name!')
