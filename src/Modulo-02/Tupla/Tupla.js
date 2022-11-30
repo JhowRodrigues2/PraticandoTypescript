808d4f0a5ac88c13f85f49173bcd310cc97269cc
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-30 09:13:37 -0300

    modulo 2-Tuplas

---
+++ src/Modulo-02/Tupla/Tupla.js
@@ -0,0 +1,16 @@
+"use strict";
+// Tuplas simples 
+let pessoa;
+pessoa = ['Jhow Rodrigues', 'front end developer', 29];
+// Outra forma de usar Tuplas
+let pessoa2 = ['Jhow Rodrigues', 'front end developer', 29];
+// tuplas com spreak operator - Indica que o todos possui o mesmo type, independente do tamanho da Tupla
+let listFrutas = ['mamão', 'Maçã', 'Melancia'];
+console.log(...listFrutas);
+// Lista Hetegênea de Tupla
+let listaFrutas2 = [5, true, ...listFrutas];
+// Tuplas em funções
+function listarPessoas(nomes, idades) {
+    return [...nomes, ...idades];
+}
+let result = listarPessoas(['Jhow', 'Maria'], [34, 68]);
---
+++ src/Modulo-02/Tupla/Tupla.ts
@@ -0,0 +1,22 @@
+
+// Tuplas simples 
+let pessoa: [string,string,number];
+pessoa=['Jhow Rodrigues','front end developer', 29]; 
+
+// Outra forma de usar Tuplas
+let pessoa2:[Nome:string, posicao:string, idade:number]=['Jhow Rodrigues','front end developer', 29]; 
+
+// tuplas com spreak operator - Indica que o todos possui o mesmo type, independente do tamanho da Tupla
+let listFrutas: [string,...string[]] = ['mamão','Maçã','Melancia']
+console.log(...listFrutas)
+
+// Lista Hetegênea de Tupla
+let listaFrutas2:[number,boolean,...string[]]=[5,true,...listFrutas]
+
+// Tuplas em funções
+
+function listarPessoas(nomes:string[], idades:number[]){
+return[...nomes,...idades]
+
+}
+let result = listarPessoas(['Jhow','Maria'],[34,68])
