052079c9ed9d22b5cbad2a2e301a6d5798de7599
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-12-02 09:02:59 -0300

    Modulo-02 NullUndefined

---
+++ src/Modulo-02/NullUndefined/NullUndefined.ts
@@ -0,0 +1,11 @@
+// exemplo null
+
+let variavelTest = null;
+console.log(variavelTest)
+console.log(typeof (variavelTest))
+
+//exemplo undefined:
+
+let variavelTestUndefined = undefined
+console.log(variavelTestUndefined)
+console.log(typeof (variavelTestUndefined))
