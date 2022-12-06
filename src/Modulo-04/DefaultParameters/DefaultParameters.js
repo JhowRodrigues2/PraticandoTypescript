9310a2cc2b3ec398a99332e676d434b87c0c20c6
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-12-06 09:10:23 -0300

    DefaultParameters Commit

---
+++ src/Modulo-04/DefaultParameters/DefaultParameters.js
@@ -0,0 +1,14 @@
+"use strict";
+// exemple01 - desconto ja está explicito o seu valor
+function descontoCompra(preco, desconto = 0.08) {
+    return preco * (1 - desconto);
+}
+console.log(descontoCompra(100));
+//exemple02 
+function exibirMSG(msg, saudar = 'fala,pessoal!') {
+    return saudar + ' ' + msg;
+}
+//exemple03
+function exibirNome(nome, sobrenome = 'Rodrigues') {
+    return nome + ' ' + sobrenome;
+}
---
+++ src/Modulo-04/DefaultParameters/DefaultParameters.ts
@@ -0,0 +1,15 @@
+// exemple01 - desconto ja está explicito o seu valor
+function descontoCompra(preco:number, desconto=0.08){
+    return preco *(1-desconto)
+}
+console.log(descontoCompra(100))
+
+//exemple02 
+function exibirMSG(msg:string, saudar = 'fala,pessoal!'){
+    return saudar+' '+ msg
+}
+
+//exemple03
+function exibirNome(nome:string, sobrenome='Rodrigues'){
+return nome + ' ' + sobrenome
+}
